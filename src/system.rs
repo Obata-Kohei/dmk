499bd9b2b9dedbcf5cd966d903d1e08d0e108f73
Obata Kohei <ocelot.12.ok@gmail.com>
2026-01-14 19:55:55 +0900

    20260114

--- src/system.rs
+++ src/system.rs
@@ -1,14 +1,26 @@
 use bevy::prelude::*;
-
 use crate::component::{Acceleration, Velocity};
 
+pub fn acceleration_system(
+    time: Res<Time>,
+    mut query: Query<(&Acceleration, &mut Velocity)>
+) {
+    let dt = time.delta_secs();
+
+    for (a, mut v) in &mut query {
+        v.0 += a.0 * dt;
+    }
+}
 
 pub fn movement_system(
     time: Res<Time>,
-    mut query: Query< (&Acceleration, &mut Velocity, &mut Transform) >
+    mut query: Query<(&Velocity, &mut Transform)>
 ) {
-    for (a, mut v, mut tf) in &mut query {
-        v.0 = a.0 * time.delta_secs();
-        tf.translation += v.0 * time.delta_secs();
+    let dt = time.delta_secs();
+
+    for (v, mut tf) in &mut query {
+        tf.translation += v.0 * dt;  // v.0.extend(0.0)?
     }
 }
+
+
